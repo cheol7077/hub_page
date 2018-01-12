fe9e9db09c7d1fb1c96802ca528afed772edf2df
cheol <cheol7077@gmail.com>
2018-01-12 09:27:09 +0900

    멤버아이디 확인을 위한 getMember함수 추가

--- hub_page/src/board/dao/BoardDAO.java
+++ hub_page/src/board/dao/BoardDAO.java
@@ -5,7 +5,9 @@ import java.util.List;
 import org.apache.ibatis.annotations.Param;
 
 import board.vo.BoardVO;
+import board.vo.MemberVO;
 
 public interface BoardDAO {
 	public List<BoardVO> getBoardList(@Param("index") int index, @Param("order") String order, @Param("time") int time);
+	public MemberVO getMember(@Param("userID") String userID);
 }
