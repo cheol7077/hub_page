b596b4aafd324242bb0e5b00d8075314b7976bb4
cheol <cheol7077@gmail.com>
2018-01-16 14:37:34 +0900

    새로 만든 컨트롤러를 인식하지 못함. #5
    어노테이션 생략으로 인해 오류 발생.

---
+++ hub_page/src/com/scrap/service/ScrapService.java
@@ -0,0 +1,32 @@
+package com.scrap.service;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Param;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.board.vo.BoardVO;
+import com.scrap.dao.ScrapDAO;
+
+@Service
+public class ScrapService {
+	@Autowired
+	private SqlSessionTemplate sqlSessionTemplate;
+	
+	public int scrapBoard(@Param("uid") int uid, @Param("bid") int bid) {
+		ScrapDAO scrapDao = sqlSessionTemplate.getMapper(ScrapDAO.class);
+		return scrapDao.scrapBoard(uid, bid);
+	}
+	
+	public int checkScrap(@Param("uid") int uid, @Param("bid") int bid) {
+		ScrapDAO scrapDao = sqlSessionTemplate.getMapper(ScrapDAO.class);
+		return scrapDao.checkScrap(uid, bid);
+	}
+	
+	public List<BoardVO> getBoardList(@Param("uid") int uid) {
+		ScrapDAO scrapDao = sqlSessionTemplate.getMapper(ScrapDAO.class);
+		return scrapDao.getBoardList(uid);
+	}
+}
