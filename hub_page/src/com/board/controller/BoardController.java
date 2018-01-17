01103482330e60af5e8a34574bf36bf88b58e407
cheol <cheol7077@gmail.com>
2018-01-17 16:27:45 +0900

    #6
    @RequestParam(value="siteList[]")

--- hub_page/src/com/board/controller/BoardController.java
+++ hub_page/src/com/board/controller/BoardController.java
@@ -6,6 +6,7 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -15,80 +16,39 @@ import com.board.vo.BoardVO;
 @Controller
 public class BoardController {
 	final int INIT = 0;
-	
+
 	@Autowired
 	BoardService boardService;
 
+	@RequestMapping("/main.do")
+	public void main() {
+	}
+	
 	@RequestMapping("/board.do")
-	public String board(@RequestParam("index") int index, @RequestParam("order") String order, @RequestParam("time") int time,
-			@RequestParam("site") String site, HttpServletRequest request) {
-		List<BoardVO> list = boardService.getBoardList(index, order, time, site);
-		String temp="";
-		
-		for(int i=0; i<list.size(); i++) {
-			temp = list.get(i).getContent();
-			temp = temp.replace("\\", "::");
-			String contents[] = temp.split("::");
-			temp = "";
-			
-			for(int j=0; j<contents.length; j++) {
-			contents[j] = contents[j].replace("::","");
-			if(contents[j].startsWith("http")&&((contents[j].endsWith("jpg")||(contents[j].endsWith("bmp"))||(contents[j].endsWith("gif"))||(contents[j].endsWith("png"))||(contents[j].endsWith("jpeg"))
-					||(contents[j].endsWith("JPG"))||(contents[j].endsWith("JPEG"))||(contents[j].endsWith("BMP"))||(contents[j].endsWith("GIF"))||(contents[j].endsWith("PNG")) ))){
-				contents[j] = "<img width = 200 height = 200 src =\'" +contents[j] + "\'/>" + "";
-			}else if(contents[j].startsWith("https://www.youtube.com")||(contents[j].startsWith("https://media"))){
-				contents[j] = "<iframe src =\'" +contents[j] + "\'></iframe>"  ;
-			}else if(contents[j].startsWith("http")){
-				contents[j] = "<a href =\'" +contents[j] + "\'></a>" ;						
-			}
-			temp += contents[j];
-			}
-			
-			list.get(i).setContent(temp);
-		}
-		
+	public String board(@RequestParam("index") int index, @RequestParam("order") String order,
+			@RequestParam("time") int time, @RequestParam(value="siteList[]") List<String> siteList, Model model) {
+		List<BoardVO> boardList = boardService.getBoardList(index, order, time, siteList);
+		model.addAttribute("boardList", boardList);
+		return "boardAdd";
+	}
+
+	@RequestMapping("/boardSearch.do")
+	public String boardSearch(@RequestParam("index") int index, @RequestParam("order") String order,
+			@RequestParam("time") int time, @RequestParam("searchKeyword") String searchKeyword,
+			@RequestParam("searchOption") String searchOption, HttpServletRequest request) {
+		List<BoardVO> list = boardService.getBoardSearch(index, order, time, searchKeyword, searchOption);
+		String temp = "";
+
 		request.setAttribute("list", "");
 		request.setAttribute("list", list);
-	    
 		if (index == INIT)
 			return "board";
 		else
 			return "boardAdd";
 	}
-	
-	@RequestMapping("/boardSearch.do")
-	public String boardSearch(@RequestParam("index") int index, @RequestParam("order") String order, @RequestParam("time") int time,
-			@RequestParam("searchKeyword") String searchKeyword,@RequestParam("searchOption") String searchOption, HttpServletRequest request) {
-		List<BoardVO> list = boardService.getBoardSearch(index, order, time, searchKeyword, searchOption);
-		String temp="";
-		
-		for(int i=0; i<list.size(); i++) {
-			temp = list.get(i).getContent();
-			temp = temp.replace("\\", "::");
-			String contents[] = temp.split("::");
-			temp = "";
-			
-			for(int j=0; j<contents.length; j++) {
-			contents[j] = contents[j].replace("::","");
-			if(contents[j].startsWith("http")&&((contents[j].endsWith("jpg")||(contents[j].endsWith("bmp"))||(contents[j].endsWith("gif"))||(contents[j].endsWith("png"))||(contents[j].endsWith("jpeg"))
-					||(contents[j].endsWith("JPG"))||(contents[j].endsWith("JPEG"))||(contents[j].endsWith("BMP"))||(contents[j].endsWith("GIF"))||(contents[j].endsWith("PNG")) ))){
-				contents[j] = "<img width = 200 height = 200 src =\'" +contents[j] + "\'/>" + "";
-			}else if(contents[j].startsWith("https://www.youtube.com")||(contents[j].startsWith("https://media"))){
-				contents[j] = "<iframe src =\'" +contents[j] + "\'></iframe>"  ;
-			}else if(contents[j].startsWith("http")){
-				contents[j] = "<a href =\'" +contents[j] + "\'></a>" ;						
-			}
-			temp += contents[j];
-			}
-			
-			list.get(i).setContent(temp);
-		}
-		
-		request.setAttribute("list", "");
-		request.setAttribute("list", list);
-		if (index == INIT)
-			return "board";
-		else
-			return "boardAdd";
+
+	@RequestMapping("/checkbox.do")
+	public void checkbox(@RequestParam("list") String list, Model model) {
+		System.out.println(list);
 	}
 }
