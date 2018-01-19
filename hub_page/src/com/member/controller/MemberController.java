0f130aaac774a4b4c007ee0fc6ab6738f6058fed
cheol <cheol7077@gmail.com>
2018-01-19 16:05:23 +0900

    #8
    @ResponseBody 추가

--- hub_page/src/com/member/controller/MemberController.java
+++ hub_page/src/com/member/controller/MemberController.java
@@ -1,52 +1,64 @@
 package com.member.controller;
 
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.member.service.MemberService;
 import com.member.vo.MemberVO;
 
 @Controller
 public class MemberController {
+	final String R_MAIN = "redirect:/main.do";
+	final String F_MAIN = "forward:/main.do";
+	final int SUCCESS = 1;
+	final int FAIL = 0;
 	@Autowired
 	MemberService memberService;
 
 	@RequestMapping("/signupUI.do")
-	public String signupUI() {
-		return "signupUI";
+	public void signupUI() {
 	}
 
 	@RequestMapping("/signup.do")
 	public String signup(MemberVO mv) {
 		memberService.insertMember(mv);
 
-		return "main";
+		return R_MAIN;
 	}
 
 	@RequestMapping("/memberCheck.do")
-	public void check(@RequestParam("userID") String userID, HttpServletRequest request) {
+	@ResponseBody
+	public String memberCheck(@RequestParam("userID") String userID, Model model) {
+		String check = "";
 		MemberVO mv = memberService.getMember(userID);
-		request.setAttribute("member", mv);
+		if(mv != null)
+			check = "FAIL";
+		return check;
 	}
 
 	@RequestMapping("/login.do")
 	public String login(@RequestParam("userID") String userID, @RequestParam("password") String password,
-			HttpSession session) {
+			HttpSession session, Model model) {
 		int check = memberService.checkMember(userID, password);
-		if (check != 0)
+		if (check != 0) {
 			session.setAttribute("sessionId", userID);
-		
-		return "main";
+			model.addAttribute("checkLogin", SUCCESS);
+		} else {
+			model.addAttribute("checkLogin", FAIL);
+		}
+
+		return F_MAIN;
 	}
-	
+
 	@RequestMapping("/logout.do")
 	public String logout(HttpSession session) {
 		session.invalidate();
-		return "main";
+		return R_MAIN;
 	}
 }
